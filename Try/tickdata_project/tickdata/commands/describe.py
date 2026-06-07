d15ddcaebaf119d9648043b92516b320bc140042
Jaron Schut <jaron.schut@gmail.com>
2026-06-07 17:12:50 +0200

    File Completer naar zijn eigen file

--- Try/tickdata_project/tickdata/commands/describe.py
+++ Try/tickdata_project/tickdata/commands/describe.py
@@ -8,31 +8,7 @@ from tickdata.data.csvfile import CsvFile
 from tickdata.util.csvutils import load_tickdata
 from tickdata.util.display import human_size, format_datetime
 from prompt_toolkit import prompt
-from prompt_toolkit.completion import Completer, Completion
-
-class FileCompleter(Completer):
-    """Tab-completes filenames and directories."""
-
-    def get_completions(self, document, complete_event):
-        text = document.text_before_cursor
-        dirname = os.path.dirname(text)
-        prefix = os.path.basename(text)
-        search_dir = dirname if dirname else '.'
-
-        try:
-            entries = sorted(os.listdir(search_dir))
-        except OSError:
-            return
-
-        for entry in entries:
-            if entry.startswith(prefix):
-                full = os.path.join(dirname, entry) if dirname else entry
-                suffix = os.sep if os.path.isdir(full) else ''
-                yield Completion(
-                    entry + suffix,
-                    start_position=-len(prefix),
-                    display_meta='dir' if os.path.isdir(full) else 'file'
-                )
+from tickdata.data.filecompleter import FileCompleter
 
 def prompt_filename(message='Enter filename'):
     """Prompt for a filename with tab-autocomplete."""
