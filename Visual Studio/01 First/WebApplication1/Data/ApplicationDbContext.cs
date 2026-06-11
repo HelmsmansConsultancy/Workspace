06dff02b6c7382bf9cfd15a69bc8145fa006e384
Jaron Schut <jaron.schut@gmail.com>
2026-06-11 23:52:31 +0200

    good riddance

---
+++ Visual Studio/01 First/WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    public DbSet<WebApplication1.Models.Joke> Joke { get; set; } = default!;
+    }
+}
---
+++ Visual Studio/01 First/WebApplication1/Models/Joke.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public int Setup { get; set; }
+        public int Punchline { get; set; }
+
+        public Joke()
+        {
+                
+        }
+    }
+}
